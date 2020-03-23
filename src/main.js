38337b45de872ba3dc82af80fca106ccc2955d03
Your Name <1035375298@qq.com>
2020-03-23 16:51:46 +0800

    第一天任务

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 导入核心js包
+import ElementUI from 'element-ui'
+// 导入核心样式
+import 'element-ui/lib/theme-chalk/index.css'
+// 导入路由实例
+import router from './router/index.js'
+
+// 注册element组件库
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  // 挂载路由实例
+  router
 }).$mount('#app')
---
+++ src/views/Login.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="login-container">
+    <el-card class="my-card">
+      <img src="../assets/logo_index.png" alt />
+      <el-form>
+        <el-form-item>
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input
+            v-model="loginForm.code"
+            placeholder="请输入验证码"
+            style="width:240px;margin-right:8px"
+          ></el-input>
+          <el-button>发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox :value="true">我已阅读并同意协议</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" style="width:100%">登 录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "my-login",
+  data() {
+    return {
+      loginForm: {
+        mobile: "",
+        code: ""
+      }
+    };
+  }
+};
+</script>
+
+<style scoped lang='less'>
+// 全屏容器
+.login-container {
+  position: absolute;
+  left: 0;
+  top: 0;
+  // 让高度和宽度直接继承窗口大小
+  width: 100%;
+  height: 100%;
+  background: url(../assets/login_bg.jpg) no-repeat center / cover;
+  .my-card {
+    width: 400px;
+    height: 350px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -60%);
+    img {
+      display: block;
+      width: 200px;
+      margin: 0 auto 20px;
+    }
+  }
+}
+</style>
